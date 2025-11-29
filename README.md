13889245ce4c91ed1f498fce26c012ce18a4ed67
jun konann <s24C1079LG@s.chibakoudai.jp>
2025-11-29 20:59:13 +0900

    len number

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# コマンド
+# 中央値を返すコマンド
 ![test](https://github.com/jumakonana/rbsys25_a1/actions/workflows/test.yml/badge.svg)
 
-robosys第1課題
+中央値を返すコマンド
 
-このソフトウェアパッケージは, 3条項BSDライセンスの下, 再頒布および使用が許可されます.
+
 
 ## テスト環境
 - Ubuntu 22.04 LTS
 
+このソフトウェアパッケージは, 3条項BSDライセンスの下, 再頒布およ
+び使用が許可されます.
 © 2025 Kyohei Tanaka
--- median.py
+++ median.py
@@ -2,15 +2,29 @@
 # SPDX-License-Identifer: BSD-3-Clause
 
 import sys
-a = sys.stdin.read()
+#a = sys.stdin.read()
 
 i = j = 0
 
-while i < len(a):
-    if a[i] != '\n':
-      print(a[i])
-      j += 1
-    i += 1
+#while i < len(a):
+ #   if a[i] != '\n':
+  #    #print(a[i])
+   #   j += 1
+    #i += 1
 
-print(len(a) - j)
+n = x = c = 0
 
+for line in sys.stdin:
+    try:
+        n = int(line)
+        #print(n)
+    except:
+        n = float(line)
+    c += 1
+    if c == 2:
+        x = n
+
+#print(len(a) - j)
+print(c)
+#print(n)
+print(x)
